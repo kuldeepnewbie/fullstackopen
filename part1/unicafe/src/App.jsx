2614cac7f2561106bfc39ebfb36773754bb46d1f
kuldeepnewbie <kuldeep7234@gmail.com>
2024-01-06 22:29:19 +0530

    code refractor and changes

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,47 +6,75 @@ const Button = ({ click, name }) => {
   )
 };
 
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine text='All' value={props.total} />
+      <StatisticLine text='Average' value={((props.good * 1) + (props.neutral * 0) + (props.bad * -1)) / props.total} />
+      <StatisticLine text='Percentage' value={(props.good / props.total) * 100} />
+      </tbody>
+    </table>
+      // <p>Good {props.good}</p>
+      // <p>Neutral {props.neutral}</p>
+      // <p>Bad {props.bad}</p>
+      // <p>All {props.total}</p>
+      // <p>Average {((props.good * 1) + (props.neutral * 0) + (props.bad * -1)) / props.total}</p>
+      // <p>Percentage {(props.good / props.total) * 100}%</p>
+  )
+}
+
 const App = () => {
   //save click of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total,setTotal] = useState(0);
-  const [average, setAverage] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const giveFeedback = (type) => {
-    let feedbackValue = 0;
+    const totalSum = total + 1;
     switch (type) {
       case 'Good':
-        feedbackValue = feedbackValue + 1;
         setGood(good + 1);
         break;
       case 'Neutral':
-        feedbackValue = feedbackValue + 0;
         setNeutral(neutral + 1);
         break;
       case 'Bad':
-        feedbackValue = feedbackValue - 1;
         setBad(bad + 1);
         break;
       default:
         break;
     }
-    setTotal(total + feedbackValue);
-    setAverage(total + feedbackValue / (good + neutral + bad + 1));
+    setTotal(totalSum);
   }
   return (
     <div>
-      <h4>Give Feedback</h4>
+      <h3>Give Feedback</h3>
       <Button name="Good" click={() => giveFeedback('Good')} />
       <Button name="Neutral" click={() => giveFeedback('Neutral')} />
       <Button name="Bad" click={() => giveFeedback('Bad')} />
-      <h4>Statistics</h4>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Total {total}</p>
-      <p>Feedback {average}</p>
+      <h3>Statistics</h3>
+      <Statistics good={good} bad={bad} neutral={neutral} total={total} />
     </div>
   )
 }
