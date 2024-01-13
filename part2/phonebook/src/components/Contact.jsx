05d032e0cf2e0ab3ca9ea4041687133d118bb16f
kuldeepnewbie <kuldeep7234@gmail.com>
2024-01-14 00:29:00 +0530

    changes

--- part2/phonebook/src/components/Contact.jsx
+++ part2/phonebook/src/components/Contact.jsx
@@ -1,14 +1,5 @@
 /* eslint-disable react/prop-types */
-import phoneServices from "../services/person";
-const Contact = ({name,number,id})=>{
-
-    const deleteItem = (id)=>{
-        if (window.confirm("Do you really want to Delete?")) {
-            phoneServices.deleteItem(id).then(result=>{
-
-            })          
-        }
-    }
+const Contact = ({name,number,id,deleteItem})=>{
     return (<tr>
         <td>{name}</td>
         <td>{number}</td>
--- part2/phonebook/src/components/Phonebook.jsx
+++ part2/phonebook/src/components/Phonebook.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import Contact from "./Contact";
-const Phonebook = ({persons, filterName}) =>{
-    const filteredArray = persons.filter(ele=>ele.name.toLowerCase().includes(filterName.toLowerCase()))
+const Phonebook = ({persons, filterName, deleteItem}) =>{
+    console.log(filterName,"sadkjaskldj")
+    const filteredArray = persons.filter(ele=>ele?.name.toLowerCase().includes(filterName.toLowerCase()))
     const filteredContacts = filteredArray.map((person)=>{
-       return <Contact name={person.name} phnumberone={person.number} key={person.id} id={person.id} />
+       return <Contact name={person.name} number={person.number} id={person.id}  key={person.id} deleteItem={deleteItem} />
     })
     const unFilteredContacts = filteredArray.map((person)=>{
-       return <Contact name={person.name} number={person.number} key={person.id} id={person.id}/>
+       return <Contact name={person.name} number={person.number} id={person.id}  key={person.id} deleteItem={deleteItem}/>
     })
     const contactsToShow = filterName ? filteredContacts : unFilteredContacts
     return (
