a74fed14c72e75966d56e19fac007f9a01d00fe3
kuldeepnewbie <kuldeep7234@gmail.com>
2024-01-20 01:00:13 +0530

    changes

--- part2/dataforcountries/src/components/Result.jsx
+++ part2/dataforcountries/src/components/Result.jsx
@@ -1,35 +1,67 @@
 /* eslint-disable react/prop-types */
 import View from "./View";
+import { useState } from "react";
 
 const Result = (props) => {
   let filteredArray = [];
-  
+  const [showData, setShowData] = useState(false);
+  const [finalData, setFinalData] = useState([]);
+  const mystyle = {
+    'display': "flex",
+    'align-items': "center"
+  };
+  const handleButtonClick = (ele) => {
+    const index = filteredArray.findIndex(eles => eles.fifa == ele.fifa);
+    setFinalData([filteredArray[index]]);
+    setShowData(true)
+  }
+  const clearData = ()=>{
+    console.log("hekk")
+    setShowData(false)
+  }
   if (props.country) {
     filteredArray = props.country.filter(ele => ele?.name?.common.toLowerCase().includes(props.filter.toLowerCase()));
-    console.log(filteredArray)
-    if (filteredArray.length >= 10) {
-      return <p>Too many matches, specify another filter</p>;
-    } else if (filteredArray.length <= 10 && filteredArray.length > 1) {
-      return (
-        <>
-          {filteredArray.map(ele => <h1 key={ele.ccn3}>{ele.name.common}</h1>)}
-        </>
-      );
-    } else {
-      return (
-        <>
-          {filteredArray.map(ele => (
-            <View
-              key={ele.ccn3}
-              name={ele.name.common}
-              flags={ele.flags}
-              capital={ele.capital}
-              area={ele.area} languages={ele.languages}
-            />
-          ))}
-        </>
-      );
-    }
+  }
+  if (showData) {
+    return (
+      <View
+        key={finalData[0].ccn3}
+        name={finalData[0].name.common}
+        flags={finalData[0].flags}
+        capital={finalData[0].capital}
+        area={finalData[0].area}
+        languages={finalData[0].languages}
+        clearData = {clearData}
+      />
+    );
+  }
+  if (filteredArray.length == 1) {
+    return (
+      <View
+        key={filteredArray[0].ccn3}
+        name={filteredArray[0].name.common}
+        flags={filteredArray[0].flags}
+        capital={filteredArray[0].capital}
+        area={filteredArray[0].area}
+        languages={filteredArray[0].languages}
+        clearData = {()=>clearData}
+      />
+    );
+  }
+  if (filteredArray.length >= 10) {
+    return <p>Too many matches, specify another filter</p>;
+  }
+  if ((filteredArray.length <= 10 && filteredArray.length > 1) && !showData) {
+    return (
+      <>
+        {filteredArray.map(ele => (
+          <div style={mystyle} key={ele.ccn3}>
+            <h1>{ele.name.common}</h1>
+            <button onClick={() => handleButtonClick(ele)}>show</button>
+          </div>
+        ))}
+      </>
+    )
   }
 };
 
